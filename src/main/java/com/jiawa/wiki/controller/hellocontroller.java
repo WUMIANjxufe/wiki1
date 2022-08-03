5aac9d6dc35114316e85c97441abdc69ba1afa72
wumian <2202120598@stu.jxufe.edu.cn>
2022-08-03 21:39:59 +0800

    3-3 IDEA数据库插件配置

--- src/main/java/com/jiawa/wiki/controller/hellocontroller.java
+++ src/main/java/com/jiawa/wiki/controller/hellocontroller.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,12 @@ import java.util.Map;
 
 @RestController
 public class hellocontroller {
+    @Value("${test.hello}")
+    private String testHello;
      @GetMapping("/hello")
         public String hello(){
              System.out.println("helloworld");
-             return "hello world";
+             return "hello world"+testHello;
          }
       @PostMapping("/hello/post")
       public String helloPost(String name){
