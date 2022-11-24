4e30851aa209964675b0d19856863cea98c6e4e9
wumian <2202120598@stu.jxufe.edu.cn>
2022-11-24 20:34:10 +0800

    4-7制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    倚动实验室工作证明系统
+  </a-layout-footer>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
